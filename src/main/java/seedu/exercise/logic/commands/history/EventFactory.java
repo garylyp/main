c7be53230e55f51f05ae01c05fba4a58eee64743
garylyp <garylyp@hotmail.com>
2019-10-13 21:42:57 +0800

    Update comments

--- src/main/java/seedu/exercise/logic/commands/history/EventFactory.java
+++ src/main/java/seedu/exercise/logic/commands/history/EventFactory.java
@@ -1,15 +1,11 @@
 package seedu.exercise.logic.commands.history;
 
-import java.util.logging.Logger;
-
-import seedu.exercise.commons.core.LogsCenter;
 import seedu.exercise.logic.commands.AddExerciseCommand;
 import seedu.exercise.logic.commands.ClearCommand;
 import seedu.exercise.logic.commands.DeleteExerciseCommand;
 import seedu.exercise.logic.commands.EditCommand;
 import seedu.exercise.logic.commands.UndoableCommand;
 import seedu.exercise.logic.commands.exceptions.CommandException;
-import seedu.exercise.model.ModelManager;
 import seedu.exercise.model.ReadOnlyExerciseBook;
 import seedu.exercise.model.exercise.Exercise;
 
@@ -21,14 +17,12 @@ public class EventFactory {
     public static final String MESSAGE_COMMAND_NOT_UNDOABLE =
             "The command \'%1$s\' cannot be stored as an undoable event.";
 
-    private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
-
     /**
      * Generates an Event object that can execute the behaviour of a given Command as well
      * as its opposite behaviour.
      *
-     * @param command a command to be represented with using an Event object
-     * @return an Event that can be undone or redone
+     * @param command a {@code Command} to be represented with using an Event object
+     * @return an {@code Event} that can be undone or redone
      */
     static Event commandToEvent(UndoableCommand command) throws CommandException {
         if (command instanceof AddExerciseCommand) {
@@ -46,7 +40,6 @@ public class EventFactory {
 
         } else if (command instanceof ClearCommand) {
             ReadOnlyExerciseBook exerciseBook = ((ClearCommand) command).getExerciseBookPrevious();
-            logger.info("Exercise book: " + exerciseBook);
             return new ClearEvent(exerciseBook);
 
         } else {
