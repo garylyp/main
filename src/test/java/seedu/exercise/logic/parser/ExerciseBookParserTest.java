3732be2b2409fa5b495e0b237a8ad1fefd3407dc
garylyp <garylyp@hotmail.com>
2019-10-25 10:57:13 +0800

    Remove FindCommand and FindCommandParser from Main and Test

--- src/test/java/seedu/exercise/logic/parser/ExerciseBookParserTest.java
+++ src/test/java/seedu/exercise/logic/parser/ExerciseBookParserTest.java
@@ -21,12 +21,10 @@ import seedu.exercise.logic.commands.DeleteExerciseCommand;
 import seedu.exercise.logic.commands.EditCommand;
 import seedu.exercise.logic.commands.EditCommand.EditExerciseDescriptor;
 import seedu.exercise.logic.commands.ExitCommand;
-import seedu.exercise.logic.commands.FindCommand;
 import seedu.exercise.logic.commands.HelpCommand;
 import seedu.exercise.logic.commands.ListCommand;
 import seedu.exercise.logic.parser.exceptions.ParseException;
 import seedu.exercise.model.resource.Exercise;
-import seedu.exercise.model.resource.NameContainsKeywordsPredicate;
 import seedu.exercise.testutil.exercise.EditExerciseDescriptorBuilder;
 import seedu.exercise.testutil.exercise.ExerciseBuilder;
 import seedu.exercise.testutil.exercise.ExerciseUtil;
@@ -69,14 +67,6 @@ public class ExerciseBookParserTest {
     public void parseCommand_exit() throws Exception {
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
-    }
-
-    @Test
-    public void parseCommand_find() throws Exception {
-        List<String> keywords = Arrays.asList("foo", "bar", "baz");
-        FindCommand command = (FindCommand) parser.parseCommand(
-            FindCommand.COMMAND_WORD + " " + keywords.stream().collect(Collectors.joining(" ")));
-        assertEquals(new FindCommand(new NameContainsKeywordsPredicate(keywords)), command);
     }
 
     @Test
